0185cd7c1e4e7f7916c2656b250e4b4eeebe38b1
matheus-baraujo <37562463+matheus-baraujo@users.noreply.github.com>
2025-05-13 13:05:27 -0300

    comecei a versão mobile do projeto

--- src/components/others/footer/index.jsx
+++ src/components/others/footer/index.jsx
@@ -30,40 +30,69 @@ const index = () => {
           <></>
           :
           <div className={styles.cta}>
-            <h2>Coloque sua marca em movimento</h2>
-            <p>VEM CO-CRIAR COM A JU!</p>
+            <h2>Se fez sentido para você, a gente grava um começo.</h2>
+
+            <div className={styles.msgButton}>
+              <p>Estou pronta e empolgada em poder fazer 
+                parte da sua história conectando autenticidade, 
+                verdade e ajudando a despertar o desejo dos 
+                seus consumidores.
+              </p>
+              <button>Me chama <br/> no Whats!</button>
+            </div>
+
+            <div className={styles.socials2}>
+              <h2>Obrigada!</h2>
+              <div className={styles.icons}>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={info[0][0]} className={info[0][1]} />
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={info[1][0]} className={info[1][1]} />
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={info[2][0]} className={info[2][1]} />
+                </a>
+              </div>
+            </div>
+
+
+            <button className={styles.top}>Voltar ao topo</button>
           </div>
+
+          
       }
 
       <div className={styles.section}>
 
       {
         pathname == '/contato' ?
-          <></>
+          <>
+            <div className={styles.socials}>
+              <div className={styles.info}>
+                <p>Onde me encontrar</p>
+
+                <div className={styles.icons}>
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={info[0][0]} className={info[0][1]} />
+                  </a>
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={info[1][0]} className={info[1][1]} />
+                  </a>
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={info[2][0]} className={info[2][1]} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </>
           :
           <Navbar footer={true}/>
       }
 
         
 
-        <div className={styles.socials}>
-          <div className={styles.info}>
-            <p>Onde me encontrar</p>
-
-            <div className={styles.icons}>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={info[0][0]} className={info[0][1]} />
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={info[1][0]} className={info[1][1]} />
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={info[2][0]} className={info[2][1]} />
-              </a>
-            </div>
-          </div>
-
-        </div>
+        
 
         <div className={styles.copyright}>
           <p>Política de privacidade</p>
--- src/components/others/navbar/index.jsx
+++ src/components/others/navbar/index.jsx
@@ -7,8 +7,8 @@ import styles from './styles.module.css'
 import Button from '@/components/others/navbar/button'
 import Button2 from '@/components/others/navbar/button2'
 
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const redirect = (url) => {
   window.location.href = '/'+url
@@ -20,6 +20,9 @@ const index = ({footer}) => {
   return (
     <div className={styles.navbar}>
 
+      
+      <img className={styles.logoMobile} src="/logos/logoMenu.png" alt="logo" onClick={() => window.location.href = '/' }/>
+
       <div className={styles.menu}>
 
         <img className={styles.logo} src="/logos/logoMenu.png" alt="logo" onClick={() => window.location.href = '/' }/>
@@ -28,9 +31,12 @@ const index = ({footer}) => {
         <Button text={"Cases"} onClickHandle={redirect} />
         <Button text={"InCompany"} onClickHandle={redirect} />
         <Button text={"Contato"} onClickHandle={redirect} />
-
         <Button2 text={"Orçamento"} onClickHandle={redirect} footer={footer}/>
+        
       </div>
+      
+      <button className={styles.sandwich}><FontAwesomeIcon icon={faBars} className={'fas fa-bars'} /></button>
+      
 
     </div>
   )
